1c351a8b905056e1c15d91b230018f4981da10ab
Lucas Ivisson <lucasivisson1@gmail.com>
2023-06-02 08:13:48 -0300

    ✨ introduce algorithm to create a new user

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -6,6 +6,7 @@ import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
+import { api } from '@/lib/axios'
 
 const registerFormSchema = z.object({
   username: z
@@ -40,7 +41,16 @@ export default function Register() {
     }
   }, [router.query?.username, setValue])
 
-  async function handleRegister(data: RegisterFormData) {}
+  async function handleRegister(data: RegisterFormData) {
+    try {
+      await api.post('/users', {
+        username: data.username,
+        name: data.name,
+      })
+    } catch (err) {
+      console.log(err)
+    }
+  }
 
   return (
     <Container>
