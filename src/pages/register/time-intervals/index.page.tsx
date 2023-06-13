a9947c69d7eed25b3a99718034afd05f9cb7054a
Lucas Ivisson <lucasivisson1@gmail.com>
2023-06-13 14:28:16 -0300

    🚧 working with availability component

---
+++ src/pages/register/time-intervals/index.page.tsx
@@ -0,0 +1,39 @@
+import { Checkbox, Heading, MultiStep, Text, TextInput } from '@ignite-ui/react'
+import { Container, Header } from '../styles'
+import {
+  IntervalBox,
+  IntervalDay,
+  IntervalInputs,
+  IntervalItem,
+  IntervalsContainer,
+} from './styles'
+
+export default function TimeIntervals() {
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Quase lá</Heading>
+        <Text>
+          Defina o intervalo de horários que você está disponível em cada dia da
+          semana.
+        </Text>
+
+        <MultiStep size={4} currentStep={3} />
+      </Header>
+
+      <IntervalBox as="form">
+        <IntervalsContainer>
+          <IntervalItem>
+            <IntervalDay>
+              <Checkbox />
+              <Text>Segunda-feira</Text>
+            </IntervalDay>
+            <IntervalInputs>
+              <TextInput size="sm" type="time" step={60} />
+            </IntervalInputs>
+          </IntervalItem>
+        </IntervalsContainer>
+      </IntervalBox>
+    </Container>
+  )
+}
