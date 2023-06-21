a558882b0d10d1eaaa7cc45953315d16c7b4e8de
Lucas Ivisson <lucasivisson1@gmail.com>
2023-06-21 11:25:52 -0300

    ✨ creating time intervals on database

--- src/pages/api/users/time-intervals.api.ts
+++ src/pages/api/users/time-intervals.api.ts
@@ -1,6 +1,15 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { getServerSession } from 'next-auth'
 import { buildNextAuthOptions } from '../auth/[...nextauth].api'
+import { prisma } from '@/lib/prisma'
+
+type timeIntervalsBodySchema = {
+  intervals: {
+    weekDay: number
+    startTimeInMinutes: number
+    endTimeInMinutes: number
+  }[]
+}
 
 export default async function handler(
   req: NextApiRequest,
@@ -16,5 +25,24 @@ export default async function handler(
     buildNextAuthOptions(req, res),
   )
 
-  return res.json(session)
+  if (!session) {
+    return res.status(401).end()
+  }
+
+  const { intervals } = req.body as timeIntervalsBodySchema
+
+  await Promise.all(
+    intervals.map((interval) => {
+      return prisma.userTimeInterval.create({
+        data: {
+          week_day: interval.weekDay,
+          time_start_in_minutes: interval.startTimeInMinutes,
+          time_end_in_minutes: interval.endTimeInMinutes,
+          user_id: session.user?.id,
+        },
+      })
+    }),
+  )
+
+  return res.status(201).end()
 }
