cfdccb1d59d1337259080eb45822d44677f03299
Lucas Ivisson <lucasivisson1@gmail.com>
2023-06-26 12:17:32 -0300

    🚧 creating schedule page

---
+++ src/pages/schedule/[username]/index.page.tsx
@@ -0,0 +1,60 @@
+import { Heading, Avatar, Text } from '@ignite-ui/react'
+import { Container, UserHeader } from './styles'
+import { GetStaticPaths, GetStaticProps } from 'next'
+import { prisma } from '@/lib/prisma'
+
+interface ScheduleProps {
+  user: {
+    name: string
+    bio: string
+    avatarUrl: string
+  }
+}
+
+export default function Schedule({
+  user: { name, bio, avatarUrl },
+}: ScheduleProps) {
+  return (
+    <Container>
+      <UserHeader>
+        <Avatar src={avatarUrl}></Avatar>
+        <Heading>{name}</Heading>
+        <Text>{bio}</Text>
+      </UserHeader>
+    </Container>
+  )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: 'blocking',
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const username = String(params?.username)
+
+  const user = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (!user) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {
+      user: {
+        name: user.name,
+        bio: user.bio,
+        avatarUrl: user.avatar_url,
+      },
+    },
+    revalidate: 60 * 60 * 24, // page will be updated each day after first request
+  }
+}
