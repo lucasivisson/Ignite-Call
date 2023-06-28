cb6f39518eac31f00ff8c345953a4760aa7d98c5
Lucas Ivisson <lucasivisson1@gmail.com>
2023-06-28 10:49:07 -0300

    🚧 working with styles of calendar

--- src/components/Calendar/styles.ts
+++ src/components/Calendar/styles.ts
@@ -41,12 +41,56 @@ export const CalendarActions = styled('div', {
       color: '$gray100',
     },
 
-    '$:focus': {
-      boxShadow: '0 0 0 2px',
+    '&:focus': {
+      boxShadow: '0 0 0 2px $colors$gray100',
     },
   },
 })
 
-export const CalendarBody = styled('table', {})
+export const CalendarBody = styled('table', {
+  width: '100%',
+  fontFamily: '$default',
+  borderSpacing: '0.25rem',
+  tableLayout: 'fixed',
 
-export const CalendarDay = styled('button', {})
+  'thead th': {
+    color: '$gray200',
+    fontWeight: '$medium',
+    fontSize: '$sm',
+  },
+
+  'tbody:before': {
+    content: '.',
+    lineHeight: '0.75rem',
+    display: 'block',
+    color: '$gray800',
+  },
+
+  'tbody td': {
+    boxSizing: 'bordex-box',
+  },
+})
+
+export const CalendarDay = styled('button', {
+  all: 'unset',
+  width: '100%',
+  aspectRatio: '1 / 1',
+  background: '$gray600',
+  textAlign: 'center',
+  cursor: 'pointer',
+  borderRadius: '$sm',
+
+  '&:disabled': {
+    background: 'none',
+    cursor: 'default',
+    opacity: 0.4,
+  },
+
+  '&:not(:disabled):hover': {
+    background: '$gray500',
+  },
+
+  '&:focus': {
+    boxShadow: '0 0 0 2px $colors$gray100',
+  },
+})
