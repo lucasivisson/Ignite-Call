2dce3314b977955ec886c5e094d08c8a97fdcff1
Lucas Ivisson <lucasivisson1@gmail.com>
2023-06-29 17:01:16 -0300

    🚧 working with selection of day of calendar

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -21,7 +21,12 @@ type CalendarWeek = {
 
 type CalendarWeeks = CalendarWeek[]
 
-export function Calendar() {
+type CalendarProps = {
+  selectedDate: Date | null
+  onDateSelected: (date: Date) => void
+}
+
+export function Calendar({ selectedDate, onDateSelected }: CalendarProps) {
   const [currentDate, setCurrentDate] = useState(() => {
     return dayjs().set('date', 1)
   })
@@ -74,7 +79,7 @@ export function Calendar() {
         return { date, disabled: true }
       }),
       ...daysInMonthArray.map((date) => {
-        return { date, disabled: false }
+        return { date, disabled: date.endOf('day').isBefore(new Date()) }
       }),
       ...nextMonthFillArray.map((date) => {
         return { date, disabled: true }
@@ -99,8 +104,6 @@ export function Calendar() {
 
     return calendarWeeks
   }, [currentDate])
-
-  console.log(calendarWeeks)
 
   const shortWeekDays = getWeekDays({ short: true })
 
@@ -135,7 +138,10 @@ export function Calendar() {
                 {days.map(({ date, disabled }) => {
                   return (
                     <td key={date.toString()}>
-                      <CalendarDay disabled={disabled}>
+                      <CalendarDay
+                        onClick={() => onDateSelected(date.toDate())}
+                        disabled={disabled}
+                      >
                         {date.get('date')}
                       </CalendarDay>
                     </td>
